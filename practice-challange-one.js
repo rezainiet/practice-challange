29632a387fd7f39927e9c54859328f8c1fcc6947
rezainiet <masudrezaog@gmail.com>
2022-01-27 22:51:16 +0600

    initial commit

---
+++ practice-challange-one.js
@@ -0,0 +1,153 @@
+// Grade Numbers 
+
+
+var dGrade = 50;
+var cGrade = 60;
+var bGrade = 70;
+var aGrade = 80;
+var aPlusGrade = 90;
+
+// Students marks 
+
+var alia = 95;
+var dalia = 66;
+var chaliya = 80;
+var maliya = 59;
+var liliya = 47;
+var jalaiya = 77;
+
+
+// Finding Grade of students
+
+
+// Alias Grade Point 
+
+if (alia >= aPlusGrade) {
+    console.log('Alia got A+ grade');
+}
+else if (alia >= aGrade) {
+    console.log('Alia got A Grade');
+}
+else if (alia >= bGrade) {
+    console.log('Alia got B Grade');
+}
+else if (alia >= cGrade) {
+    console.log('Alia got C Grade');
+}
+else if (alia >= dGrade) {
+    console.log('Alia got D Grade');
+}
+else if (alia >= fGrade) {
+    console.log('Alia got F Grade');
+}
+
+
+// Dalias Grade Point 
+
+if (dalia >= aPlusGrade) {
+    console.log('Dalia got A+ grade');
+}
+else if (dalia >= aGrade) {
+    console.log('Dalia got A Grade');
+}
+else if (dalia >= bGrade) {
+    console.log('Dalia got B Grade');
+}
+else if (dalia >= cGrade) {
+    console.log('Dalia got C Grade');
+}
+else if (dalia >= dGrade) {
+    console.log('Dalia got D Grade');
+}
+else if (dalia >= fGrade) {
+    console.log('Dalia got F Grade');
+}
+
+// Chaliyas Grade Point 
+
+if (chaliya >= aPlusGrade) {
+    console.log('Chaliya got A+ grade');
+}
+else if (chaliya >= aGrade) {
+    console.log('Chaliya got A Grade');
+}
+else if (chaliya >= bGrade) {
+    console.log('Chaliya got B Grade');
+}
+else if (chaliya >= cGrade) {
+    console.log('Chaliya got C Grade');
+}
+else if (chaliya >= dGrade) {
+    console.log('Chaliya got D Grade');
+}
+else if (chaliya >= fGrade) {
+    console.log('Chaliya got F Grade');
+}
+
+
+// Maliyas Grade Point 
+
+
+if (maliya >= aPlusGrade) {
+    console.log('Maliya got A+ grade');
+}
+else if (maliya >= aGrade) {
+    console.log('Maliya got A Grade');
+}
+else if (maliya >= bGrade) {
+    console.log('Maliya got B Grade');
+}
+else if (maliya >= cGrade) {
+    console.log('Maliya got C Grade');
+}
+else if (maliya >= dGrade) {
+    console.log('Maliya got D Grade');
+}
+else if (maliya >= fGrade) {
+    console.log('Maliya got F Grade');
+}
+
+// Liliyas Grade Point 
+
+
+if (liliya >= aPlusGrade) {
+    console.log('Liliya got A+ grade');
+}
+else if (liliya >= aGrade) {
+    console.log('Liliya got A Grade');
+}
+else if (liliya >= bGrade) {
+    console.log('Liliya got B Grade');
+}
+else if (liliya >= cGrade) {
+    console.log('Liliya got C Grade');
+}
+else if (liliya >= dGrade) {
+    console.log('Liliya got D Grade');
+}
+else if (liliya >= fGrade) {
+    console.log('Liliya got F Grade');
+}
+
+
+// Jalaiyas Grade point 
+
+
+if (jalaiya >= aPlusGrade) {
+    console.log('Jalaiya got A+ grade');
+}
+else if (jalaiya >= aGrade) {
+    console.log('Jalaiya got A Grade');
+}
+else if (jalaiya >= bGrade) {
+    console.log('Jalaiya got B Grade');
+}
+else if (jalaiya >= cGrade) {
+    console.log('Jalaiya got C Grade');
+}
+else if (jalaiya >= dGrade) {
+    console.log('Jalaiya got D Grade');
+}
+else if (jalaiya >= fGrade) {
+    console.log('Jalaiya got F Grade');
+}
