2881b58642eb351b21f0a387c152406d3390e5fd
rezainiet <masudrezaog@gmail.com>
2022-01-27 23:06:05 +0600

    Challange Two Added

---
+++ practice-challange-two.js
@@ -0,0 +1,11 @@
+var signal = 'green';
+
+if (signal == 'red') {
+    console.log("We are in Danger");
+}
+else if (signal == 'yellow') {
+    console.log('We have to wait');
+}
+else if (signal == 'green') {
+    console.log('We can cross the road now!');
+}
